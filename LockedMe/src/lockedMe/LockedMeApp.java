322d77bc1cf376d2bd1da4690cd5b4c0dcef710c
Gian Carlo Uy <gian.carlo.uy.2@gmail.com>
2021-06-29 10:10:38 -0500

    Finalized project and provided fixes and increased program readability

--- LockedMe/src/lockedMe/LockedMeApp.java
+++ LockedMe/src/lockedMe/LockedMeApp.java
@@ -1,25 +1,17 @@
 package lockedMe;
 
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class LockedMeApp {
 
 	final static String Directory = "C:\\Users\\gianc\\Desktop\\Simplilearn\\Phase1\\Submissions\\LockedMe";
+	final static File dir = new File(Directory + "\\lockedMeFiles");
 	static Scanner scan = new Scanner(System.in);
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		LockedMeApp run = new LockedMeApp();
 		run.welcome();
@@ -38,7 +30,7 @@ public class LockedMeApp {
 		System.out.println();
 	}
 	
-	public void mainMenu()
+	public void mainMenu() throws IOException
 	{
 		System.out.println("=====================");
 		System.out.println("--    Main Menu    --");
@@ -47,31 +39,45 @@ public class LockedMeApp {
 		System.out.println("What would you like to do today?");
 		System.out.println("1. Show files");
 		System.out.println("2. Show file operations");
-		System.out.println("3. Close the app");
-		System.out.println();
-		mainMenuHandler();		
-	}
+		System.out.println("3. Close the app" + "\n");
+		System.out.println("Please choose 1, 2 or 3:");
 	
-	public void mainMenuHandler()
-	{
-		System.out.println("Please choose 1, 2, or 3: ");
-		String choice = scan.nextLine();
-		
-		switch (choice)
+		int choice = scan.nextInt();
+		try
 		{
-		case "1": LockedMeApp.listFiles();
-					break;
-		case "2": fileOperations();
-		case "3": System.out.println("Closing the App...");
-		          System.out.println("Thank You for using LockedMe App!");
-		          System.exit(0);
-		          break;
-		default:  System.out.println("Invalid choice, please choose 1, 2, or 3 only.");
+			if(choice == 1)
+			{
+				LockedMeApp.listFiles();
+				mainMenu();	
+			}
+			if (choice == 2)
+			{
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("Opening File Operations Menu..." + "\n");
+				fileOperations();
+			}
+			if (choice == 3)
+			{
+				System.out.println("Closing the App...");
+				System.out.println("Thank You for using LockedMe App!");
+				System.exit(0);
+			}
+			if (choice !=1 && choice != 2 && choice != 3)
+			{
+				throw new CustomLockedMeException("Invalid entry!");
+			}
 		}
-		mainMenu();
+		catch (CustomLockedMeException e)
+		{
+			e.getStackTrace();
+			System.out.println("__________________________________________________" + "\n");
+			System.out.println(e);
+			System.out.println("Invalid input! Please restart the App.");
+			System.out.println("__________________________________________________" + "\n");
+		}
 	}
 	
-	public void fileOperations()
+	public void fileOperations() throws CustomLockedMeException, IOException
 	{
 		System.out.println("=======================");
 		System.out.println("==  File Operations  ==");
@@ -81,219 +87,149 @@ public class LockedMeApp {
 		System.out.println("2. Delete a file");
 		System.out.println("3. Search for a file");
 		System.out.println("4. Return to Main Menu");
-		System.out.println();
-		fileOperationsHandler();
-	}
-	
-	public void fileOperationsHandler()
-	{
-		System.out.println("Please choose 1, 2, 3, or 4: ");
-		String choice2 = scan.nextLine();
-		switch (choice2)
-		{
-		case "1": LockedMeApp.addFile();
-					break;
-		case "2": LockedMeApp.deleteFile();
-					break;
-		case "3": LockedMeApp.searchFile();
-					break;
-		case "4": mainMenu();
-					break;
-		}
-		fileOperations();
-	}
-	
-	public static void listFiles()
-	{
-		System.out.println("Showing files in ascending order: ");
-		System.out.println("__________________________________________________");
-		File[] file = new File(Directory).listFiles();
-		Set<String> sortedFiles = new TreeSet<>();
-		for (File filesToSort : file)
-		{
-			if (!filesToSort.isFile())
+		System.out.println("\n" + "Please choose 1, 2, 3, or 4: ");
+		int choice2 = scan.nextInt();
+		try
 			{
-				continue;
+			if (choice2 == 1)
+			{
+				System.out.println("Please enter the file name you want to create:");
+				String nameOfFileToAdd = scan.next().trim().toLowerCase();
+				addFile(nameOfFileToAdd);
+				fileOperations();
 			}
-			sortedFiles.add(filesToSort.getName());
-		}
-		sortedFiles.forEach(System.out::println);
-		System.out.println("__________________________________________________");
-	}
-	
-	public static void addFile() throws InvalidPathException
-	{
-		{ try {
-			boolean fileCreated = false;			
-			System.out.println("Enter the file name that you want to create:");		
-		//	Scanner in = new Scanner (System.in);	
-			String filename= scan.next();
-		
-			File f= new File (filename);
-			if (f.exists()) 
+			if (choice2 == 2)
 			{
-				System.out.println(f.getName() + " file already exists!");
+				System.out.println("Please enter the file name you want to delete:");
+				String nameOfFileToDelete = scan.next().trim();
+				deleteFile(nameOfFileToDelete);
+				fileOperations();
 			}
-			else
+			if (choice2 == 3)
 			{
-				System.out.println("Creating file now...");
-				fileCreated = f.createNewFile();
-				if (fileCreated) 
-				{
-					System.out.println("You have sucessfully created a new file  :"+"'" +f.getName()+"'"+ " in the directory");
-				}
-				else
-				{
-					System.out.println("Unable to create file.");
-				}
+				System.out.println("Please enter the file name you want to search:");
+				String nameOfFileToSearch = scan.next().trim();
+				searchFile(nameOfFileToSearch);
+				fileOperations();
 			}
-			
-//			if (f.createNewFile()) {
-//				System.out.println( "You have sucessfully created a new file  :"+"'" +f.getName()+"'"+ " in the directory");
-//			}
-//			else {
-//				System.out.println( f.getName()+  ":File already exists ");	
-//			}	
-	//		menuList2();
-	//		scan.close();
-			} catch(Exception e) {  
-				e.printStackTrace(); 
-				System.out.println("File is not created ");
-			}		
-		}
-//		{ try {
-//			File file = new File(Directory);
-//			if (file.createNewFile())
-//			{
-//				System.out.println("New file created successfully!");
-//			}
-//			else
-//			{
-//				if (file.exists())
-//				{
-//					DataInputStream dis = new DataInputStream(System.in);
-//					FileOutputStream fos = new FileOutputStream(Directory);
-//					BufferedOutputStream bout = new BufferedOutputStream(fos, 1024);
-//					System.out.println("Please enter the file name you want to create (enter @ symbol right after file name without space between): ");
-//					char c;
-//					while()
-//				}
-//			}
-	//		boolean fileCreated = false;			
-//			System.out.println("Please enter the file name you want to create: ");		
-//			String addName= scan.next();
-//			File addFile= new File (addName);
-//			if (addFile.exists()) 
-//			{
-//				System.out.println(addFile.getName() + " file already exists!");
-//			}
-//			else
-//			{
-//				System.out.println("Creating file now...");
-//				//fileCreated = addFile.createNewFile();
-//				if (fileCreated) 
-//				{
-//					System.out.println("You have sucessfully created a new file  :"+"'" +addFile.getName()+"'"+ " in the directory");
-//				}
-//				else
-//				{
-//					System.out.println("Unable to create file.");
-//				}
-//			}
-			
-//			if (f.createNewFile()) {
-//				System.out.println( "You have sucessfully created a new file  :"+"'" +f.getName()+"'"+ " in the directory");
-//			}
-//			else {
-//				System.out.println( f.getName()+  ":File already exists ");	
-//			}	
-//			scan.close();
-//			} catch(Exception e) {  
-//				e.printStackTrace(); 
-//				System.out.println("Unable to create file.");
-//			}		
-//		}
-		
-//		System.out.println("Please enter the file name you want to create: ");
-//		String fileDir = Directory;
-//		Path addPath = Paths.get(fileDir);
-//		if (!Files.exists(addPath))
-//		{
-//			System.out.println("File does not exists!");
-//			return;
-//		}
-//		
-//		String newFileDir = Directory + "/" + addPath.getFileName();
-//		int g = 0;
-//		while (Files.exists(Paths.get(newFileDir)))
-//		{
-//			g++;
-//			newFileDir = Directory + "/" + g + "_" + addPath.getFileName();
-//		}
-//		try
-//		{
-//			Files.copy(addPath, Paths.get(newFileDir));
-//		}
-//		catch (IOException e)
-//		{
-//			System.out.println("Unable to create file.");
-//		}
-	}
-	
-	public static void deleteFile()
-	{
-		System.out.println("Please enter the file name you want to delete: ");
-		String deleteName = scan.nextLine();
-		String deleteFile = Directory + "/" + deleteName;
-		Path deletePath = Paths.get(deleteFile);
-		
-		try 
-		{
-			Files.delete(deletePath);
-		}
-		catch (IOException e)
-		{
-			System.out.println("File was not deleted.");
-		}
-		finally
-		{
-			System.out.println("File deleted!");
-		}
-	}
-	
-	public static void searchFile()
-	{
-		System.out.println("Please enter the file name you want to search: ");
-		String searchFile = scan.nextLine();
-		
-		File[] file = new File(Directory).listFiles();
-		ArrayList<String> fileList = new ArrayList<>();
-		for (File fileToSearch : file)
-		{
-			if (!fileToSearch.isFile())
+			if (choice2 == 4)
 			{
-				continue;
+				System.out.println("\n" + "Returning to Main Menu..." + "\n");
+				mainMenu();
 			}
-			fileList.add(fileToSearch.getName());
-		}
-		boolean findResult = false;
-		for (String fileNameToBeSearched : fileList)
-		{
-			if (fileNameToBeSearched.equals(searchFile))
+			if (choice2 != 4 && choice2 !=1 && choice2 != 2 && choice2 != 3)
 			{
-				findResult = true;
+				throw new CustomLockedMeException("Invalid entry!");
 			}
 		}
-		if (findResult)
+		catch (CustomLockedMeException e)
 		{
-			System.out.println("File " + searchFile + " was found!");
+			e.getStackTrace();
+			System.out.println("__________________________________________________" + "\n");
+			System.out.println(e);
+			System.out.println("Invalid input! Please restart the App.");
+			System.out.println("__________________________________________________" + "\n");
+		}
+	}	
+
+	static void listFiles()
+	{
+		if (dir.list().length == 0)
+		{
+			System.out.println("__________________________________________________" + "\n");
+			System.out.println("The directory is empty.");
 		}
 		else
 		{
-			System.out.println(searchFile + " was not found.");
+			String[] filesInDirectory = dir.list();
+			System.out.println("\n" + "Showing files in ascending order: ");
+			System.out.println("__________________________________________________" + "\n");
+			Arrays.sort(filesInDirectory);
+			for (String sortedFileNames : filesInDirectory)
+			{
+				System.out.println(sortedFileNames);
+			}
 		}
-	}	
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("Returning to Main Menu..."+ "\n");
+//		System.out.println("__________________________________________________" + "\n");
+	}
+
+	void addFile(String nameOfFileToAdd) throws IOException
+	{
+		File rootFilePath = new File(dir + "\\" + nameOfFileToAdd);
+		String[] filesInDirectory = dir.list();
+		for (String fileToAdd : filesInDirectory)
+		{
+			if (nameOfFileToAdd.equalsIgnoreCase(fileToAdd))
+			{
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("\n" + nameOfFileToAdd + " file already exists!" + "\n");
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("Returning to File Operations Menu..." + "\n");
+				return;
+			}
+		}
+		System.out.println("\n" + "Creating the file now..." + "\n");
+		rootFilePath.createNewFile();
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("You have sucessfully created "+ nameOfFileToAdd + " in the directory" + "\n");
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("Returning to File Operations Menu..." + "\n");
+	}
+	
+	void deleteFile(String nameOfFileToDelete) throws CustomLockedMeException
+	{	
+		File rootFilePath = new File(dir + "\\" + nameOfFileToDelete);
+		String[] filesInDirectory = dir.list();
+		for (String fileToDelete : filesInDirectory)
+		{
+			if (nameOfFileToDelete.equals(fileToDelete))
+			{
+				System.out.println("\n" + "Searching for the file now...");
+				System.out.println("File located." + "\n" + "Deleting the file now..." + "\n");
+				rootFilePath.delete();
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("You have sucessfully deleted "+ nameOfFileToDelete + " in the directory" + "\n");
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("Returning to File Operations Menu..." + "\n");
+				return;
+			}
+		}
+		System.out.println("\n" + "Searching for the file now..." + "\n");
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("\n" + "Unable to delete the file." + "\n" + "Please make sure the spelling is correct (Case-ensitive) " + "\n" + "Otherwise, make sure the file exists first." + "\n");
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("Returning to File Operations Menu..." + "\n");
+	}
+
+	void searchFile(String nameOfFileToSearch)
+	{
+		File rootFilePath = new File(dir + "\\" + nameOfFileToSearch);
+		String[] filesInDirectory = dir.list();
+		for (String fileToSearch : filesInDirectory)
+		{
+			if (nameOfFileToSearch.equals(fileToSearch))
+			{
+				System.out.println("\n" + "Searching for the file now..." + "\n");
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println(nameOfFileToSearch + " was found in the directory!" + "\n");
+				System.out.println("__________________________________________________" + "\n");
+				System.out.println("Returning to File Operations Menu..." + "\n");
+				return;
+			}
+		}
+		System.out.println("\n" + "Searching for the file now..." + "\n");
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("\n" + nameOfFileToSearch + " does not exists." + "\n");
+		System.out.println("__________________________________________________" + "\n");
+		System.out.println("Returning to File Operations Menu..." + "\n");
+	}
+	class CustomLockedMeException extends Exception
+	{
+		CustomLockedMeException(String CLME)
+		{
+			super(CLME);
+		}
+	}
 }
-
-
- 
